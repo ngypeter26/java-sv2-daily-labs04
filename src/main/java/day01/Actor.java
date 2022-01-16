b3e1d2fe08eba6a3efef549422d7b2c6970dcae2
ngypeter26 <ngypeter26@gmail.com>
2022-01-16 22:06:06 +0100

    konzultációk week04

---
+++ src/main/java/day01/Actor.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Actor {
+    private String name;
+    private int birthyear;
+
+    public Actor(String name, int birthyear) {
+        this.name = name;
+        this.birthyear = birthyear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthyear() {
+        return birthyear;
+    }
+}
+
+
